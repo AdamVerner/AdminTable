c1f257b387e97b07d587c1ea95269715c7e578fb
Adam Verner <averner@averner.cz>
2024-09-17 16:36:15 +0200

    proper authentication handling

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,8 @@ set -x
 cd ui && yarn build && cd ..
 
 # copy ui into package
-rm -r table_api/ui
-cp -r ui/dist table_api/ui
+rm -rf admin_table/ui
+cp -r ui/dist admin_table/ui
 
 # build python wheel and dist
 poetry build
--- ui/src/layout/navbar/UserButton.tsx
+++ ui/src/layout/navbar/UserButton.tsx
@@ -1,28 +1,68 @@
-import { IconChevronRight } from '@tabler/icons-react';
-import { Avatar, Group, rem, Text, UnstyledButton } from '@mantine/core';
+import {IconChevronRight, IconLogout} from '@tabler/icons-react';
+import {Avatar, Group, Loader, Menu, rem, Text, UnstyledButton} from '@mantine/core';
 import classes from './UserButton.module.css';
+import {useNavigate} from "react-router-dom";
+import dataService, {useGetData} from "@/services/data.service";
 
 export function UserButton() {
+  const navigate = useNavigate();
+  const [data, isLoading] = useGetData(
+    async () => await dataService.getUserInfo(),
+    []
+  );
+
+  if (isLoading || !data) {
+    return (
+      <Loader/>
+    );
+  }
+
   return (
-    <UnstyledButton className={classes.user}>
+      <Menu
+        withArrow
+        width={300}
+        position="right"
+        transitionProps={{ transition: 'pop' }}
+        withinPortal
+        trigger="click-hover"
+      >
+        <Menu.Target>
+    <UnstyledButton w="100%" p='md' className={classes.user}>
       <Group>
         <Avatar
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+          src={data.user?.avatar ?? 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png'}
           radius="xl"
         />
 
         <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
-            Harriette Spoonlicker
+          {data.user?.name ? (
+              <>
+          <Text size="sm" fw={500}>{data.user?.name}</Text>
+              <Text c="dimmed" size="xs">
+                {data.user.email}
           </Text>
+              </>
 
-          <Text c="dimmed" size="xs">
-            hspoonlicker@outlook.com
+          ): (
+              <Text  size="sm" fw={500}>
+                {data.user.email}
           </Text>
+          )
+          }
         </div>
 
         <IconChevronRight style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
       </Group>
-    </UnstyledButton>
+    </UnstyledButton></Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item
+            leftSection={<IconLogout style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+            onClick={()=>{navigate('/logout')}}
+          >
+            Logout
+          </Menu.Item>
+        </Menu.Dropdown>
+      
+      </Menu>
   );
 }
